84942e15ea796815beb34a4a7ecaf16b7861c729
Pixoll <pixoll.gamer@gmail.com>
2026-07-08 19:43:50 -0400

    feat: build k2 and pemb

---
+++ src/build_trees.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "complementary/utils.hpp"
+#include "k2tree/buildtree.h"
+#include "k2tree/compressleaves.h"
+#include "k2tree/kTree.h"
+#include "sdsl/pemb.hpp"
+
+const auto GRAPH_EXT = ".pg";
+const auto K2_EXT = ".k2";
+const auto PEMB_EXT = ".pe";
+
+inline std::vector<std::filesystem::path> get_graph_file_paths() {
+    namespace fs = std::filesystem;
+
+    const fs::path graphs_dir_path = fs::path(__builtin_FILE()).parent_path().parent_path() / "graphs";
+    std::vector<fs::path> graph_file_paths;
+
+    for (const auto &entry: fs::directory_iterator(graphs_dir_path)) {
+        if (entry.is_regular_file() && entry.path().extension() == GRAPH_EXT) {
+            graph_file_paths.push_back(entry.path());
+        }
+    }
+
+    return graph_file_paths;
+}
+
+inline void build_k2tree(const std::filesystem::path &file_path) {
+    namespace fs = std::filesystem;
+
+    fs::path out_file_path = file_path;
+    out_file_path.replace_extension(K2_EXT);
+
+    if (fs::exists(out_file_path)) {
+        return;
+    }
+
+    std::cout << "k2 tree file for " << file_path.stem() << " not found, building" << std::endl;
+
+    std::ifstream infile(file_path);
+    if (!infile) {
+        std::cerr << "failed to open " << file_path << std::endl;
+        exit(1);
+    }
+
+    std::ofstream outfile(out_file_path, std::ios::binary);
+    if (!outfile) {
+        std::cerr << "failed to open " << out_file_path << std::endl;
+        exit(1);
+    }
+
+    std::cout << "mapping edges for k2 tree" << std::endl;
+
+    int32_t num_vertices;
+    int32_t num_edges;
+    infile >> num_vertices >> num_edges;
+
+    outfile.write(reinterpret_cast<const char *>(&num_vertices), sizeof(int32_t));
+    outfile.write(reinterpret_cast<const char *>(&num_edges), sizeof(int64_t));
+
+    int32_t src;
+    int32_t dst;
+    int32_t current_node = -1;
+
+    for (int32_t i = 0; i < num_edges; i++) {
+        infile >> src >> dst;
+
+        while (current_node < src) {
+            current_node++;
+            const int32_t marker = -(current_node + 1);
+            outfile.write(reinterpret_cast<const char *>(&marker), sizeof(int32_t));
+        }
+
+        const int32_t val = dst + 1;
+        outfile.write(reinterpret_cast<const char *>(&val), sizeof(int32_t));
+    }
+
+    while (current_node < num_vertices - 1) {
+        current_node++;
+        const int32_t marker = -(current_node + 1);
+        outfile.write(reinterpret_cast<const char *>(&marker), sizeof(int32_t));
+    }
+
+    infile.close();
+    outfile.close();
+
+    constexpr int argc_bt = 7;
+    const char *argv_bt[] = {
+        ".",
+        // graph file path
+        out_file_path.c_str(),
+        // basename
+        out_file_path.c_str(),
+        // K1, must be 4
+        "4",
+        // K2, must be 2
+        "2",
+        // max level K1, 5 = smaller, 6 = biggest
+        "6",
+        // speeddup, 22 if large graph
+        "21",
+    };
+
+    std::cout << "building k2 tree" << std::endl;
+
+    if (build_tree(argc_bt, argv_bt) != 0) {
+        std::cerr << "build_tree() failed" << std::endl;
+        exit(1);
+    }
+
+    constexpr int argc_cl = 3;
+    const char *argv_cl[] = {
+        ".",
+        out_file_path.c_str(),
+        "4000000",
+    };
+
+    std::cout << "compressing k2 tree leaves" << std::endl;
+
+    if (compress_leaves(argc_cl, argv_cl) != 0) {
+        std::cerr << "compress_leaves() failed" << std::endl;
+    }
+}
+
+inline void build_pemb(const std::filesystem::path &file_path) {
+    namespace fs = std::filesystem;
+
+    fs::path out_file_path = file_path;
+    out_file_path.replace_extension(PEMB_EXT);
+
+    if (fs::exists(out_file_path)) {
+        return;
+    }
+
+    std::cout << "planar embedding for " << file_path.stem() << " not found, building" << std::endl;
+
+    std::cout << "building graph for planar embedding" << std::endl;
+    Graph graph = read_graph_from_file(file_path.c_str());
+
+    std::cout << "building planar embedding" << std::endl;
+    sdsl::pemb pemb(graph);
+
+    std::ofstream outfile(out_file_path, std::ios::binary);
+    pemb.serialize(outfile);
+    outfile.close();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
+#include <cmath>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
-#include "k2tree/kTree.h"
-#include "sdsl/pemb.hpp"
+#include "build_trees.hpp"
+
 namespace fs = std::filesystem;
+
+uint32_t exp_pow(const uint32_t base, const uint32_t pow) {
+    uint32_t result = 1;
+    for (uint32_t i = 0; i < pow; i++)
+        result *= base;
+    return result;
+}
 
 int main() {
     const fs::path graphs_dir_path = fs::path(__builtin_FILE()).parent_path().parent_path() / "graphs";
@@ -12,7 +21,7 @@ int main() {
     std::error_code ec;
 
     for (const auto &entry: fs::directory_iterator(graphs_dir_path, ec)) {
-        if (entry.is_regular_file()) {
+        if (entry.is_regular_file() && entry.path().extension() == GRAPH_EXT) {
             graph_file_paths.push_back(entry.path());
         }
     }
@@ -22,7 +31,10 @@ int main() {
         exit(1);
     }
 
-    std::cout << graph_file_paths.size() << std::endl;
+    for (const auto &file_path: graph_file_paths) {
+        build_k2tree(file_path);
+        build_pemb(file_path);
+    }
 
     return 0;
 }
