f0508757d605c5976cfec8549bf73444f63d76ff
Pixoll <pixoll.gamer@gmail.com>
2026-07-08 01:05:56 -0400

    feat: add sdsl

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
+#include <filesystem>
 #include <iostream>
+#include <vector>
+
+namespace fs = std::filesystem;
+
 int main() {
-    std::cout << "67" << std::endl;
+    const fs::path graphs_dir_path = fs::path(__builtin_FILE()).parent_path().parent_path() / "graphs";
+    std::vector<fs::path> graph_file_paths;
+    std::error_code ec;
+
+    for (const auto &entry: fs::directory_iterator(graphs_dir_path, ec)) {
+        if (entry.is_regular_file()) {
+            graph_file_paths.push_back(entry.path());
+        }
+    }
+
+    if (ec || graph_file_paths.empty()) {
+        std::cerr << "no graphs found at " << graphs_dir_path << std::endl;
+        exit(1);
+    }
+
+    std::cout << graph_file_paths.size() << std::endl;
+
     return 0;
 }
